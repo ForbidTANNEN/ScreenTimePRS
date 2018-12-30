28300893c51748dae271accc8d818d38d3e7adb4
Tannen Hall <Tannenhall@yahoo.com>
2018-12-30 09:11:52 -0800

    Login

--- ScreenTimePrs/LoginViewController.swift
+++ ScreenTimePrs/LoginViewController.swift
@@ -13,12 +13,36 @@ class LoginViewController: UIViewController {
     @IBOutlet fileprivate var usernameField: UITextField!
     @IBOutlet fileprivate var passwordField: UITextField!
     
+    
+    @IBAction func signIn(_ sender: UIButton){
+        print("SignIn")
+        if(self.usernameField.text == "Chris" && self.passwordField.text == "Parent"){
+            loadParentScreen()
+        }
+        else{
+            loadChildScreen()
+        }
+        
+    }
+    
+    func loadParentScreen(){
+        let storyBoard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+        let parentViewController = storyBoard.instantiateViewController(withIdentifier: "ParentViewController") as! UIViewController
+        self.present(parentViewController, animated: true, completion: nil)
+    }
+    
+    func loadChildScreen(){
+        let storyBoard: UIStoryboard = UIStoryboard(name: "Main", bundle: nil)
+        let childViewController = storyBoard.instantiateViewController(withIdentifier: "ChildViewController") as! UIViewController
+        self.present(childViewController, animated: true, completion: nil)
+    }
+    
  
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-        self.usernameField.text = "Tannen"
-        self.passwordField.text = "Dad"
+        //self.usernameField.text = "Tannen"
+        //self.passwordField.text = "Dad"
         
     }
 

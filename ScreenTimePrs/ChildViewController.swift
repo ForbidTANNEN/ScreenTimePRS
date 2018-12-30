6c2cb12200d8b89d01883cd2800e8131419b7fe1
Tannen Hall <Tannenhall@yahoo.com>
2018-12-29 19:00:44 -0800

    3 view controllers

--- ScreenTimePrs/ChildViewController.swift
+++ ScreenTimePrs/ChildViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  ChildViewController.swift
 //  ScreenTimePrs
 //
 //  Created by Tannen Hall on 12/27/18.
@@ -8,18 +8,20 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ChildViewController: UIViewController {
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    
 }
+
+
 
---
+++ ScreenTimePrs/ParentViewController.swift
@@ -0,0 +1,26 @@
+//
+//  ParentViewController.swift
+//  ScreenTimePrs
+//
+//  Created by Tannen Hall on 12/27/18.
+//  Copyright © 2018 Tannen Hall. All rights reserved.
+//
+
+import UIKit
+
+class ParentViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
+
+

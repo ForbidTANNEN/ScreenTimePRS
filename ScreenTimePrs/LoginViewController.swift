af19079a32f03f04b8c4e813e576183c3239c186
Tannen Hall <Tannenhall@yahoo.com>
2018-12-30 13:06:36 -0800

    Login is finished

--- ScreenTimePrs/LoginViewController.swift
+++ ScreenTimePrs/LoginViewController.swift
@@ -13,14 +13,26 @@ class LoginViewController: UIViewController {
     @IBOutlet fileprivate var usernameField: UITextField!
     @IBOutlet fileprivate var passwordField: UITextField!
     
+    private var childName = "Tannen"
+    private var childPass = "Child"
+    
+    private var parentName = "Chris"
+    private var parentPass = "Parent"
+    
     
     @IBAction func signIn(_ sender: UIButton){
         print("SignIn")
-        if(self.usernameField.text == "Chris" && self.passwordField.text == "Parent"){
+        if(self.usernameField.text == parentName && self.passwordField.text == parentPass){
             loadParentScreen()
         }
-        else{
+        else if(self.usernameField.text == childName && self.passwordField.text == childPass){
             loadChildScreen()
+        }
+        
+        else{
+            createAlert(title: "", message: "Incorrect Username or Password")
+            self.usernameField.text = ""
+            self.passwordField.text = ""
         }
         
     }
@@ -40,9 +52,14 @@ class LoginViewController: UIViewController {
     func createAlert (title:String, message:String){
         let alert = UIAlertController(title: title, message: message, preferredStyle: UIAlertControllerStyle.alert)
         
-        alert.addAction(UIAlertController(title: "OK", style: UIAlertActionStyle.default))
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: {(action) in alert.dismiss(animated: true, completion: nil) }))
+        
+        self.present(alert, animated: true, completion: nil)
     }
     
+    override func viewDidAppear(_ animated: Bool) {
+        
+    }
     
  
     override func viewDidLoad() {

d539e8d72720980c930dd45fa670eeef9dac54b3
Tannen Hall <Tannenhall@yahoo.com>
2019-01-02 14:13:03 -0800

    Working with images

---
+++ ScreenTimePrs/Session.swift
@@ -0,0 +1,14 @@
+//
+//  Session.swift
+//  ScreenTimePrs
+//
+//  Created by Tannen Hall on 1/2/19.
+//  Copyright © 2019 Tannen Hall. All rights reserved.
+//
+
+public class Session {
+    
+    public static let instance = Session()
+    
+}
+
